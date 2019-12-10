740acca27b1fcf1c7ce310a02d40079f858a37c2
Maxim Kugatov <maximkugatov@gmail.com>
2019-12-10 10:13:02 +0200

    feat: use set

--- backend/src/store/redis/lobbyStore.ts
+++ backend/src/store/redis/lobbyStore.ts
@@ -3,32 +3,31 @@ var redis = require("redis");
 const redisHost = process.env.REDIS_HOST;
 const client = redis.createClient(6379, redisHost);
 
+client.on("error", function (err) {
+  console.log("Error: ", err);
+});
+
 export class LobbyStore {
 
   async addToLobby(userId: string) {
 
     const lobbyKey = this.getLobbyKey();
-    client.set(lobbyKey, userId);
+    client.sadd(lobbyKey, userId, redis.print);
   }
 
   async removeFromLobby(userIds: string[]) {
 
     const lobbyKey = this.getLobbyKey();
-    client.srem(lobbyKey, userIds);
+    client.srem(lobbyKey, userIds, redis.print);
   }
 
   async getFirstFromLobby(): Promise<string> {
 
     const lobbyKey = this.getLobbyKey();
-    // const userIds: string[] = client.srandmember(lobbyKey, 1);
-    //
-    // console.log("getFirstFromLobby", userIds);
-    //
-    // if (userIds && userIds.length > 0) {
-    //   return userIds[0];
-    // }
 
-    const userIds: string[] = await new Promise((resolve, reject) => {
+    // this.flushDb();
+
+    const userId: string = await new Promise((resolve, reject) => {
 
       client.srandmember(lobbyKey,
           function (err, obj) {
@@ -44,18 +43,37 @@ export class LobbyStore {
       );
     });
 
-    console.log("getFirstFromLobby", userIds);
+    console.log("getFirstFromLobby", userId);
 
-    if (userIds && userIds.length > 0) {
-      return userIds[0];
-    }
-
-    return null;
+    return userId;
   }
 
   private getLobbyKey(): string {
     return "kugmax:chess:lobby";
   }
+
+  // private removeIfString(lobbyKey: string) {
+  //   client.type(lobbyKey,
+  //       function (err, obj) {
+  //         console.log("type err", err);
+  //         console.log("type obj", obj);
+  //
+  //         if (obj == "set") {
+  //           client.del(lobbyKey);
+  //         }
+  //       }
+  //   );
+  // }
+
+  // private flushDb() {
+  //   client.flushdb(this.logger);
+  // }
+
+  // private logger(err, obj) {
+  //   console.log("type err", err);
+  //   console.log("type obj", obj);
+  // }
+
 }
 
 
